15b9854fa274d149b9cd00675be622688ddc87be
deep.akash@prophecy.io <deep.akash@prophecy.io>
2026-07-07 07:05:32 +0000

    Update package structure and dependencies in `test` and `test2` pipelines to use prophecy-libs 2.1.17

--- pipelines/test/code/setup.py
+++ pipelines/test/code/setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 setup(
     name = 'test',
     version = '1.0',
-    packages = find_packages(include = ('test*', )) + ['prophecy_config_instances.test'],
-    package_dir = {'prophecy_config_instances.test' : 'configs/resources/test'},
+    packages = find_packages(include = ('test*', )) + ['prophecy_config_instances', 'prophecy_config_instances.test'],
+    package_dir = {'prophecy_config_instances' : 'configs/resources'},
     package_data = {'prophecy_config_instances.test' : ['*.json', '*.py', '*.conf']},
     description = 'workflow',
     install_requires = [
-'prophecy-libs==2.1.16'],
+'prophecy-libs==2.1.17'],
     entry_points = {
 'console_scripts' : [
 'main = test.pipeline:main'], },
--- pipelines/test2/code/setup.py
+++ pipelines/test2/code/setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 setup(
     name = 'test2',
     version = '1.0',
-    packages = find_packages(include = ('test2*', )) + ['prophecy_config_instances.test2'],
-    package_dir = {'prophecy_config_instances.test2' : 'configs/resources/test2'},
+    packages = find_packages(include = ('test2*', )) + ['prophecy_config_instances', 'prophecy_config_instances.test2'],
+    package_dir = {'prophecy_config_instances' : 'configs/resources'},
     package_data = {'prophecy_config_instances.test2' : ['*.json', '*.py', '*.conf']},
     description = 'workflow',
     install_requires = [
-'prophecy-libs==2.1.16'],
+'prophecy-libs==2.1.17'],
     entry_points = {
 'console_scripts' : [
 'main = test2.pipeline:main'], },
